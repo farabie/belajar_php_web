3e69144c8ff8e01bc9a2765eadd6495b4aec80c3
Muhammad Farabie <farabie024@gmail.com>
2023-11-06 15:45:40 +0700

    Header

---
+++ header-response.php
@@ -0,0 +1,9 @@
+<?php 
+
+header('Application: Belajar PHP WEB');
+header('Author: Muhammad Farabie');
+
+$client = $_SERVER['HTTP_CLIENT_NAME'];
+echo "Hello $client";
+
+?>
---
+++ header.php
@@ -0,0 +1,7 @@
+<?php 
+
+$client = $_SERVER['HTTP_CLIENT_NAME'];
+
+echo "Hello $client";
+
+?>
