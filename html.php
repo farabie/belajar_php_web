11e3c06d5cbdf9f45a3cb95833358d7ed4497565
Muhammad Farabie <farabie024@gmail.com>
2023-11-06 12:46:56 +0700

    Integrasi PHP dengan HTML

---
+++ html.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Hello HTML dengan PHP";
+$body = "Hello HTML dengan PHP";
+?>
+
+<html>
+
+<head>
+    <title>
+        <?php echo $title ?>
+    </title>
+</head>
+
+<body>
+    <h1><?php echo $body ?></h1>
+</body>
+
+</html>
