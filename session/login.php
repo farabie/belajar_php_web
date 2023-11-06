7e23f5985bc7eb3288ea4badd78cd16a7a46e19c
Muhammad Farabie <farabie024@gmail.com>
2023-11-06 16:31:22 +0700

    Menjalankan session(1) cek kondisi sessionnya

---
+++ session/login.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+// if($_SESSION['login'] == true) {
+//     header('Location: /session/member.php');
+//     exit();
+// }
+
+$error = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if ($_POST['username'] == "abie" && $_POST['password'] == "abie") {
+        //sukses
+        $_SESSION['login'] = true;
+        $_SESSION['username'] = 'abie';   
+        header('Location: /session/member.php');
+        exit();
+    } else {
+        //gagal
+        $error = "Login Gagal";
+    }
+}
+
+
+?>
+
+<html>
+
+<body>
+    <?php if ($error != "") { ?>
+        <h2>
+            <?= $error; ?>
+        </h2>
+    <?php } ?>
+    <h1>Login</h1>
+    <form action="/session/login.php" method="post">
+        <label> Username:
+            <input type="text" name="username">
+        </label>
+        <br />
+        <label> Password:
+            <input type="password" name="password">
+        </label>
+        <br />
+        <input type="submit" value="login">
+    </form>
+</body>
+
+</html>
